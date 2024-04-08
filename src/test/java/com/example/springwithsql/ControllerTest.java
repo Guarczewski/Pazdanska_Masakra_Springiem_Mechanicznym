9dcd53f951431c913e0e147b8689b1d9f044eb99
Guarczewski <guarczewski@gmail.com>
2024-04-08 17:45:34 +0200

    New Methods Used in Tests

--- src/test/java/com/example/springwithsql/ControllerTest.java
+++ src/test/java/com/example/springwithsql/ControllerTest.java
@@ -1,13 +1,12 @@
 package com.example.springwithsql;
 
 import com.example.springwithsql.Database.Entity.*;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.ResponseEntity;
 
 
 public class ControllerTest {
@@ -19,7 +18,10 @@ public class ControllerTest {
         while (true) {
 
             System.out.println("| TEST SELECT MENU: ");
-            System.out.println("| [1] -> Food API Test");
+            System.out.println("| [1] -> Food API GET Test");
+            System.out.println("| [11] -> Food API POST Test");
+            System.out.println("| [12] -> Food API PUT Test");
+            System.out.println("| [13] -> Food API PUT Test");
             System.out.println("| [2] -> Portion API Test");
             System.out.println("| [3] -> Horse API Test");
             System.out.println("| [4] -> Address API Test");
@@ -32,7 +34,10 @@ public class ControllerTest {
             System.out.println("| [0] -> Close");
 
             switch (scanner.nextInt()) {
-                case 1: FoodTest(); break;
+                case 1: FoodGetTest(); break;
+                case 11: FoodPostTest(); break;
+                case 12: FoodPutTest(); break;
+                case 13: FoodDeleteTest(); break;
                 case 2: PortionTest(); break;
                 case 3: HorseTest(); break;
                 case 4: AddressTest(); break;
@@ -47,15 +52,16 @@ public class ControllerTest {
     }
 
     public static void TestAll(){
-        FoodTest();
+        FoodGetTest();
         PortionTest();
         HorseTest();
         AddressTest();
         PersonTest();
     }
-    public static void FoodTest(){
+    // Start All Food Tests
+    public static void FoodGetTest(){
         try {
-            System.out.println("!!! Starting Food Test !!!");
+            System.out.println("!!! Starting Food Get Test !!!");
             RestTemplate restTemplate = new RestTemplate();
             String urlString = Config.URL + "Food/";
             ParameterizedTypeReference<List<Food>> parameterizedTypeReference = new ParameterizedTypeReference<>() {};
@@ -67,6 +73,80 @@ public class ControllerTest {
             if (Config.displayOutput)
                 for (Food tmp : tmpList)
                     System.out.println("->-> " + tmp.toString());
+
+        }
+        catch (Exception e) {
+            System.out.println("-> Fail:");
+            System.out.println("->-> " + e.getMessage());
+        }
+        finally {
+            System.out.println("=== ### === ### === ### ===");
+        }
+    }
+    public static void FoodPostTest(){
+        try {
+            System.out.println("!!! Starting Food POST Test !!!");
+            HttpHeaders httpHeaders = new HttpHeaders();
+            RestTemplate restTemplate = new RestTemplate();
+            String urlString = Config.URL + "Food/";
+
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+            String jsonString = "{\"name\": \"Example\", \"price\": 3.99}";
+            HttpEntity<String> httpEntity = new HttpEntity<>(jsonString, httpHeaders);
+            ResponseEntity<String> responseEntity = restTemplate.exchange(urlString, HttpMethod.POST, httpEntity, String.class);
+
+            String result = responseEntity.getBody();
+            System.out.println("->-> " + result);
+
+        }
+        catch (Exception e) {
+            System.out.println("-> Fail:");
+            System.out.println("->-> " + e.getMessage());
+        }
+        finally {
+            System.out.println("=== ### === ### === ### ===");
+        }
+    }
+    public static void FoodPutTest(){
+        try {
+            System.out.println("!!! Starting Food POST Test !!!");
+            HttpHeaders httpHeaders = new HttpHeaders();
+            RestTemplate restTemplate = new RestTemplate();
+            int idToBeChanged = 1;
+            String urlString = Config.URL + "Food/" + idToBeChanged;
+
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+            String jsonString = "{\"name\": \"Example\", \"price\": 3.99}";
+            HttpEntity<String> httpEntity = new HttpEntity<>(jsonString, httpHeaders);
+            ResponseEntity<String> responseEntity = restTemplate.exchange(urlString, HttpMethod.PUT, httpEntity, String.class);
+
+            String result = responseEntity.getBody();
+            System.out.println("->-> " + result);
+
+        }
+        catch (Exception e) {
+            System.out.println("-> Fail:");
+            System.out.println("->-> " + e.getMessage());
+        }
+        finally {
+            System.out.println("=== ### === ### === ### ===");
+        }
+    }
+    public static void FoodDeleteTest(){
+        try {
+            System.out.println("!!! Starting Food Delete Test !!!");
+            RestTemplate restTemplate = new RestTemplate();
+            int idToBeRemoved = 8;
+            String urlString = Config.URL + "Food/" + idToBeRemoved;
+            ParameterizedTypeReference<String> parameterizedTypeReference = new ParameterizedTypeReference<>() {};
+            ResponseEntity<String> responseEntity = restTemplate.exchange(urlString, HttpMethod.DELETE, null, String.class);
+            String response = responseEntity.getBody();
+
+            System.out.println("-> Success");
+
+            System.out.println("->-> " + response);
 
         }
         catch (Exception e) {
